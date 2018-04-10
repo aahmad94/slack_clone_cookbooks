8fc18fefd5c916ea1dbaaa5a19aee6f6162cbd50
aahmad94 <aka5vm@virginia.edu>
2018-04-09 20:35:57 -0700

    remove spooky

--- slack_clone/templates/slack_clone_site.rb
+++ slack_clone/templates/slack_clone_site.rb
@@ -1,4 +1,4 @@
-upstream spooky_unicorn {
+upstream slack_clone_unicorn {
   # This is the socket we configured in unicorn.rb
   server unix:/tmp/sockets/unicorn.sock; 
 }
@@ -8,8 +8,8 @@ server {
   client_max_body_size 4G;
   # server_name  *;
   
-  root <%= node['spooky']['path'] %>/public;
-  access_log  <%= node['nginx']['log_dir'] %>/spooky.access.log;
+  root <%= node['slack_clone']['path'] %>/public;
+  access_log  <%= node['nginx']['log_dir'] %>/slack_clone.access.log;
 
   location / {
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -19,13 +19,13 @@ server {
     # If you don't find the filename in the static files
     # Then request it from the unicorn server
     if (!-f $request_filename) {
-      proxy_pass http://spooky_unicorn;
+      proxy_pass http://slack_clone_unicorn;
       break;
     }
   }
 
   error_page 500 502 503 504 /500.html;
   location = /500.html {
-    root <%= node['spooky']['path'] %>/public;
+    root <%= node['slack_clone']['path'] %>/public;
   }
 }
